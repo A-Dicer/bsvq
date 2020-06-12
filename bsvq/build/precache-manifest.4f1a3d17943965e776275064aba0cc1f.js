30f6c60836295b0db5cc825dc35e3de5d1b556bd
Andrew Dicer <andrew.dicer@gmail.com>
2020-06-12 01:01:07 -0500

    heroku

---
+++ bsvq/build/precache-manifest.4f1a3d17943965e776275064aba0cc1f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd20600bab7d791a003283d47befd68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3fc28bb10ef0847c255",
+    "url": "/static/css/main.02dedf64.chunk.css"
+  },
+  {
+    "revision": "b6fff01982cc8a1769d9",
+    "url": "/static/js/2.2954e371.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.2954e371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3fc28bb10ef0847c255",
+    "url": "/static/js/main.eb4cbdcf.chunk.js"
+  },
+  {
+    "revision": "ec74739d38f76810ddb9",
+    "url": "/static/js/runtime-main.ce5b596f.js"
+  },
+  {
+    "revision": "c4e93de640ec25a43e56863c80266e19",
+    "url": "/static/media/Slide1.c4e93de6.jpg"
+  },
+  {
+    "revision": "388cf4ff70e74b0024d01af1903e9653",
+    "url": "/static/media/Slide2.388cf4ff.jpg"
+  },
+  {
+    "revision": "5c673edbc41af319cbc4f29d7fb48ebb",
+    "url": "/static/media/Slide3.5c673edb.jpg"
+  },
+  {
+    "revision": "8c41b8a754b78323789b3b4a017f4862",
+    "url": "/static/media/Slide4.8c41b8a7.jpg"
+  },
+  {
+    "revision": "7044e7e6b4aa4e9ef4b43ca98112a2d7",
+    "url": "/static/media/Slide5.7044e7e6.jpg"
+  }
+]);
