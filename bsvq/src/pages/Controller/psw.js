2eef23a0b02630a3fcbc0e531fcb77b5873e7253
andrew <andrew.dicer@gmail.com>
2020-08-19 23:01:31 -0500

    slides

--- bsvq/src/pages/Controller/psw.js
+++ bsvq/src/pages/Controller/psw.js
@@ -3,4 +3,5 @@ module.exports = {
     PUMA_SAMM2020_BIll_Crom: 'samm2020',
     PUMA_SAMM2020_Gutierrez: 'samm2020',
     PUMA_SAMM2020_Deb_Jackson: 'samm2020',
+    PUMA_SAM2020_Angela_Nicolella_Jay: 'samm2020', 
     }
