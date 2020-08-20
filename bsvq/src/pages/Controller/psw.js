ff60d58414e543d04dec84ce0f8408af3b31013b
andrew <andrew.dicer@gmail.com>
2020-08-19 22:52:45 -0500

    'slides'

--- bsvq/src/pages/Controller/psw.js
+++ bsvq/src/pages/Controller/psw.js
@@ -1,4 +1,6 @@
 module.exports = {
     bsdemo: 'demo',
-    PUMA_SAMM2020_BIll_Crom: 'samm2020'
+    PUMA_SAMM2020_BIll_Crom: 'samm2020',
+    PUMA_SAMM2020_Gutierrez: 'samm2020',
+    PUMA_SAMM2020_Deb_Jackson: 'samm2020',
     }
