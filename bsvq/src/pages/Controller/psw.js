3e6f1bdfaca3a11b29c06b21bcf45cf3ca680424
andrew <andrew.dicer@gmail.com>
2020-08-21 08:31:16 -0500

    'slides'

--- bsvq/src/pages/Controller/psw.js
+++ bsvq/src/pages/Controller/psw.js
@@ -1,7 +1,7 @@
 module.exports = {
     demo: {pwd: 'demo', v:'v.1'},
     PUMA_SAMM2020_BIll_Crom: {pwd: 'samm2020', v:'v.3'},
-    PUMA_SAMM2020_Noe_Gutierrez: {pwd: 'samm2020', v:'v.1'},
+    PUMA_SAMM2020_Noe_Gutierrez: {pwd: 'samm2020', v:'v.1.1'},
     PUMA_SAMM2020_Deb_Jackson: {pwd: 'samm2020', v:'v.2'},
     PUMA_SAMM2020_Angela_Nicolella_Jay: {pwd: 'samm2020', v:'v.1'}, 
     PUMA_SAMM2020_Lisa_Croissant: {pwd: 'samm2020', v:'v.7'}, 
@@ -10,8 +10,9 @@ module.exports = {
     PUMA_SAMM2020_Rashawnda_Baddie_Clyburn   : {pwd: 'samm2020', v:'v.2'},
     PUMA_SAMM2020_Alan_Auerbach: {pwd: 'samm2020', v:'v.1'},
     PUMA_SAMM2020_Paul_Reider: {pwd: 'samm2020', v:'v.1'},
-    PUMA_SAMM2020_Richard_Bryce: {pwd: 'samm2020', v:'draft'},
-    PUMA_SAMM2020_Julie_Tajeldin: {pwd: 'samm2020', v:'v.2'},
+    PUMA_SAMM2020_Richard_Bryce: {pwd: 'samm2020', v:'v.2'},
+    PUMA_SAMM2020_Julie_Tajeldin: {pwd: 'samm2020', v:'v.3'},
     PUMA_SAMM2020_Vivien_Gabriel: {pwd: 'samm2020', v:'v.1'},
-    PUMA_SAMM2020_Jeff_Ludwig: {pwd: 'samm2020', v:'v.2'},
+    PUMA_SAMM2020_Jeff_Ludwig: {pwd: 'samm2020', v:'v.2.1'},
+    PUMA_SAMM2020_Felicia_Townsel: {pwd: 'samm2020', v:'v.2'},
     }
