e4e542e94c29fac0fdc843813ff45530fc4b8007
andrew <andrew.dicer@gmail.com>
2020-09-09 10:44:43 -0500

    notes

---
+++ bsvq/src/pages/Controller/notes.js
@@ -0,0 +1,10 @@
+module.exports = {
+    demo: {
+        v:'v.1', 
+        notes: { 
+            1: {
+                note: 'need to fix the rate of overflow from corporate'
+            }
+        }
+    },
+}
